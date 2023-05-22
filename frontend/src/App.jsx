b61affa819b7aee20c40f143ea92de25ffc77536
Sydney Sisco <hello@sydsis.co>
2023-05-21 20:42:26 -0700

    update frontend look and feel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import futureLogo from '/future.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const [apiResponse, setApiResponse] = useState('')
+
+  const testApi = async () => {
+    const response = await fetch('/api/test')
+    const data = await response.json()
+    console.log(data)
+    setApiResponse(JSON.stringify(data, null, 2))
+  }
+
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/sydney-sisco/webapp-template" target="_blank">
+          <img src={futureLogo} className="logo" alt="future logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>webapp-template</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,8 +31,10 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logo to learn more
       </p>
+      <button onClick={testApi}>test connection to backend</button>
+      <p>{apiResponse}</p>
     </>
   )
 }
