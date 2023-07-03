3dd3b22b28d4c794559c2449bd3c54851f98031e
Sydney Sisco <hello@sydsis.co>
2023-07-03 11:53:03 -0700

    add a component for MUI tabs

---
+++ frontend/src/components/BasicTabs.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [tabs, setTabs] = useState([
+    { label: "Item One", content: "Item One" },
+    { label: "Item Two", content: "Item Two" },
+    { label: "Item Three", content: "Item Three" }
+  ]);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const addTab = (label, content) => {
+    setTabs(oldTabs => [...oldTabs, { label, content }]);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="dynamic tabs">
+          {tabs.map((tab, index) => (
+            <Tab key={index} label={tab.label} {...a11yProps(index)} />
+          ))}
+        </Tabs>
+      </Box>
+
+      {tabs.map((tab, index) => (
+        <TabPanel value={value} key={index} index={index}>
+          {tab.content}
+        </TabPanel>
+      ))}
+
+      <button onClick={() => addTab(`Item ${tabs.length + 1}`, `Item ${tabs.length + 1}`)}>Add Project</button>
+    </Box>
+  );
+}
