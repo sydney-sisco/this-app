e3ef596c3037e7859f286d975ef19b41beda7573
Sydney Sisco <hello@sydsis.co>
2023-07-03 19:38:46 -0700

    initialize indexedDB with a placeholder project

--- frontend/src/utils/ProjectsDB.js
+++ frontend/src/utils/ProjectsDB.js
@@ -2,6 +2,19 @@
 import Dexie from "dexie";
 
 const db = new Dexie("ProjectsDB");
-db.version(1).stores({ projects: '++id,name,description' });
+db.version(1).stores({ projects: '++id,title,text' });
+
+db.on('ready', function () {
+  return db.projects.count().then(function (count) {
+    if (count === 0) {
+      // Database is empty, add placeholder item
+      return db.projects.add({ title: 'Placeholder', text: 'Initial Placeholder Project' });
+    }
+  });
+});
+
+db.open().catch(function (err) {
+  console.error('Failed to open db: ' + (err.stack || err));
+});
 
 export default db;
